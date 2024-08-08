883381fe7cfe0f1489db4f34e62fae6acf831319
Jake <jakethepiglet@gmail.com>
2024-08-08 17:06:06 -0500

    Make board divs and connect logic to them

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const makeGameboard = (function () {
         if  (gameboard.at(index) === null) {
             gameboard.splice(index, 1, letter)
         }
-        //add an else in case there's already a letter there
     };
 
     //1 is playerX won, 2 is playerO won, 3 is a tie, 4 is game is still going
@@ -13,16 +12,16 @@ const makeGameboard = (function () {
         const winConditions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 3, 6, 
         1, 4, 7, 2, 5, 8, 0, 4, 8, 2, 4, 6];
         for (let i=0; i<winConditions.length; i+=3) {
-            if(gameboard.at(winConditions[i]) == "x" && 
-            gameboard.at(winConditions[i+1]) == "x" && 
-            gameboard.at(winConditions[i+2]) == "x"){
+            if(gameboard.at(winConditions[i]) == "X" && 
+            gameboard.at(winConditions[i+1]) == "X" && 
+            gameboard.at(winConditions[i+2]) == "X"){
                 return 1;
             }
         }
         for (let i=0; i<winConditions.length; i+=3) {
-            if(gameboard.at(winConditions[i]) == "o" && 
-            gameboard.at(winConditions[i+1]) == "o" && 
-            gameboard.at(winConditions[i+2]) == "o"){
+            if(gameboard.at(winConditions[i]) == "O" && 
+            gameboard.at(winConditions[i+1]) == "O" && 
+            gameboard.at(winConditions[i+2]) == "O"){
                 return 2;
             }
         } if (!gameboard.includes(null)) {
@@ -31,63 +30,69 @@ const makeGameboard = (function () {
             return 4;
         }
     }
-    
-    //add display functions using DOM manip later here
-
     return {changeLetter, checkGameOver, gameboard};
 })();
 
-const makeGameContoller = (function () {
+const displayController = () => {
+    const squares = document.querySelectorAll(".square");
+    for (let i = 0; i<squares.length; i++) {
+        squares[i].textContent = makeGameboard.gameboard[i];
+    }
+};
+
+let selectedSquare = null;
+const selectSquare = (() => {
+    const squares = document.querySelectorAll(".square");
+    const p = document.querySelector("p");
+    for (let i = 0; i<squares.length; i++) {
+        squares[i].addEventListener("click", () => {
+            selectedSquare = i;
+            makeGameContoller.playGame();
+            if (makeGameboard.checkGameOver() == 1) {
+                p.textContent = "Player X, You Won!";
+            }  else if (makeGameboard.checkGameOver() == 2) {
+                p.textContent = "Player O, You Won!";
+            } else if (makeGameboard.checkGameOver() == 3) {
+                p.textContent = "You Tied!";
+            }
+        });
+    }
+})(); 
+
+const makeGameContoller = (() => {
     function makePlayer(letter) {
         let score = 0;
         const addScore = () => score++;
         return {letter, score, addScore};
     }
 
-    const playerX = makePlayer("x");
-    const playerO = makePlayer("o");
+    const playerX = makePlayer("X");
+    const playerO = makePlayer("O");
     let turnCounter = 1;
 
     //1 is playerX won, 2 is playerO won, 3 is a tie, 4 is game is still going
     const playGame = () => {
         if (makeGameboard.checkGameOver() == 1) {
             playerX.addScore();
-            alert("Player X, you won!");
         }  else if (makeGameboard.checkGameOver() == 2) {
             playerO.addScore();
-            alert("Player O, you won!");
-        } else if (makeGameboard.checkGameOver() == 3) {
-            alert("You tied!");
-        } else {
+        }
+        if (makeGameboard.checkGameOver() == 4) {
+            let currentPlayer;
             if (turnCounter % 2 == 0) {
-                //player o turn
-                const moveO = prompt("Choose a number 0-8 to move to Player O!");
-                if (makeGameboard.gameboard[moveO] == null) {
-                    makeGameboard.changeLetter("o", moveO);
-                    turnCounter++;
-                    console.log(makeGameboard.gameboard);
-                    playGame();
-                } else {
-                    alert("Pick an empty space!");
-                    playGame();
-                }
+                currentPlayer = playerO;
             } else { 
-                //player x turn
-                const moveX = prompt("Choose a number 0-8 to move to Player X!");
-                if (makeGameboard.gameboard[moveX] == null) {
-                    makeGameboard.changeLetter("x", moveX);
-                    turnCounter++;
-                    console.log(makeGameboard.gameboard);
-                    playGame();
-                } else {
-                    alert("Pick an empty space!");
-                    playGame();
-                }
+                currentPlayer = playerX;
+            }
+            if (makeGameboard.gameboard[selectedSquare] == null) {
+                makeGameboard.changeLetter(currentPlayer.letter, selectedSquare);
+                turnCounter++;
+                displayController();
+            } else {
+                alert("Pick an empty space!");
             }
         }
     }
 
     return {playGame};
 })();
-
-makeGameContoller.playGame();
